2b19bd71a43d30939e112b0d5e97483ea878db51
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-12-30 00:35:22 +0000

    refactor: Update path calculations in constants module for correct directory structure

--- praice/core/constants.py
+++ praice/core/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 PATHS = {
-    "root": Path(__file__).absolute().parent.parent,
-    "app": Path(__file__).absolute().parent,
-    "logs": Path(__file__).absolute().parent.parent / "logs",
+    "root": Path(__file__).absolute().parent.parent.parent,
+    "app": Path(__file__).absolute().parent.parent,
+    "logs": Path(__file__).absolute().parent.parent.parent / "logs",
 }
